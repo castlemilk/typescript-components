c2ee318f795587e33806301b54f9133af151f80f
Ben Ebsworth <ben.ebsworth@gmail.com>
2019-05-20 06:16:12 +1000

    added components

---
+++ src/components/SideNav/index.tsx
@@ -0,0 +1,82 @@
+import React, { useContext, useState } from 'react';
+import { Menu, Icon, Switch, Button } from 'antd';
+import ThemeToggle from 'components/ThemeToggle';
+import { ThemeContext } from 'theme';
+import {
+  SideNavWrapper,
+  ThemeWidgetWrapper,
+  ExpandButtonWrapper,
+  MenuWrapper,
+} from './style';
+interface IExpandButton {
+  expand: boolean;
+  setExpand: Function;
+}
+
+const ExpandButton: React.FC<IExpandButton> = props => {
+  return (
+    <Button
+      type="primary"
+      onClick={() => props.setExpand(!props.expand)}
+      style={{ width: 50 }}
+    >
+      <Icon type={props.expand ? 'menu-unfold' : 'menu-fold'} />
+    </Button>
+  );
+};
+
+const ThemeWidget: React.FC = () => {
+  return <ThemeToggle />;
+};
+interface ISideNav {}
+
+const SideNav: React.FC<ISideNav> = () => {
+  const [expand, setExpand] = useState(true);
+  const [currentMenu, setCurrentMenu] = useState('OVERVIEW');
+  const { mode, theme } = useContext(ThemeContext);
+  console.log('mode:', mode);
+  return (
+    <SideNavWrapper expand={expand}>
+      <ThemeWidgetWrapper>
+        <ThemeWidget />
+      </ThemeWidgetWrapper>
+      <ExpandButtonWrapper>
+        <ExpandButton expand={expand} setExpand={setExpand} />
+      </ExpandButtonWrapper>
+      <MenuWrapper>
+        <Menu
+          className="menu"
+          inlineCollapsed={!expand}
+          theme={mode}
+          onClick={e => setCurrentMenu(e.key)}
+          defaultOpenKeys={['sub1']}
+          selectedKeys={[currentMenu]}
+          mode="inline"
+        >
+          <Menu.Item key="OVERVIEW">
+            <Icon type="pie-chart" />
+            <span>Overview</span>
+          </Menu.Item>
+          <Menu.Item key="MENU2">
+            <Icon type="desktop" />
+            <span>item 2n</span>
+          </Menu.Item>
+          <Menu.Item key="MENU3">
+            <Icon type="inbox" />
+            <span>item 3</span>
+          </Menu.Item>
+          <Menu.Item key="MENU4">
+            <Icon type="warning" />
+            <span>item 4</span>
+          </Menu.Item>
+          <Menu.Item key="MENU5">
+            <Icon type="line-chart" />
+            <span>item 5</span>
+          </Menu.Item>
+        </Menu>
+      </MenuWrapper>
+    </SideNavWrapper>
+  );
+};
+
+export default SideNav;
---
+++ src/components/SideNav/style.tsx
@@ -0,0 +1,40 @@
+import styled from 'theme';
+const Button = styled.button`
+  background: palevioletred;
+  border-radius: 3px;
+  border: none;
+  color: white;
+`;
+interface ISideNavProps {
+  expand: boolean;
+}
+export const SideNavWrapper = styled.div<ISideNavProps>`
+  max-width: ${props => (props.expand ? 140 : 80)}px;
+  grid-template-areas:
+    'theme'
+    'expander'
+    'menu';
+  grid-template-rows: 50px 50px 1fr;
+`;
+
+export const MenuWrapper = styled.div`
+  grid-area: theme;
+  max-width: 140px;
+`;
+
+export const ThemeWidgetWrapper = styled.div`
+  grid-area: expander;
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  height: 50px;
+  max-width: 140px;
+`;
+
+export const ExpandButtonWrapper = styled.div`
+  grid-area: menu;
+  justify-content: center;
+  display: flex;
+  max-width: 140px;
+  height: 50px;
+`;
